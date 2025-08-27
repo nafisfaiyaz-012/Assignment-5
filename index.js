fb582936996a6872d220769e4280e4b7da86c7ff
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-08-27 15:47:14 +0600

    copy button functionality added

--- index.js
+++ index.js
@@ -54,6 +54,35 @@ document
         alert("😭 Insufficient Coin.");
       }
     }
+
+    //for copy button functionality
+    if (event.target.classList.contains("btn-copy")) {
+      //copying the service number to the clipboard
+      const textToCopy =
+        event.target.parentNode.parentNode.childNodes[7].innerText;
+
+      const textArea = document.createElement("textarea");
+      textArea.value = textToCopy;
+
+      document.body.appendChild(textArea);
+
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+
+      //increasing copy count
+      const copyCount = Number(document.getElementById("copy-count").innerText);
+      const newCopyCount = copyCount + 1;
+      
+      //showing alert for copy button
+      alert(
+        event.target.parentNode.parentNode.childNodes[3].innerText +
+          " " +
+          event.target.parentNode.parentNode.childNodes[7].innerText +
+          " copied."
+      );
+      document.getElementById("copy-count").innerText = newCopyCount;
+    }
   });
 
 //history clear button functionality
