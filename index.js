bd99f713d9a35131038385ce76030f52fdc6a107
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-08-27 18:28:01 +0600

    button clicked image issue solved with closest method

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 document
   .getElementById("all-card-container")
   .addEventListener("click", function (event) {
-    console.log(event.target);
-    
+    // console.log(event.target.closest('.btn-call'));
+
     //for heart button it will run this
     if (event.target.classList.contains("heart-btn")) {
       const heartCounter = Number(
@@ -16,52 +16,57 @@ document
     }
 
     //for call button it will run this
-    if (event.target.classList.contains("btn-call")) {
-      const serviceName =
-        event.target.parentNode.parentNode.childNodes[5].innerText;
 
-      const serviceNumber =
-        event.target.parentNode.parentNode.childNodes[7].innerText;
-
-      //showing both alert and implementing reducing coin condition
-      const coinCount = Number(document.getElementById("coin-count").innerText);
-
-      if (coinCount >= 20) {
-        //showing alert for calling
-        alert(" 📞 Calling " + serviceName + " " + serviceNumber);
-
-        const newCoinCount = coinCount - 20;
-
-        document.getElementById("coin-count").innerText = newCoinCount;
-        //creating the history element
-        const newHistoryElement = document.createElement("div");
-
-        newHistoryElement.innerHTML = `
-        <div class="bg-[#fafafa] p-4 flex justify-between items-center rounded-3xl my-5">
-                        <div>
-                            <p class="font-semibold">${
-                              event.target.parentNode.parentNode.childNodes[3]
-                                .innerText
-                            }</p>
-                            <p>${serviceNumber}</p>
-                        </div>
-                        <div>
-                            ${new Date().toLocaleTimeString()}
-                        </div>
-        </div>
-        `;
-        document.getElementById("call-history").appendChild(newHistoryElement);
-      } else {
-        //showing alert for insufficient coin
-        alert("😭 Insufficient Coin.");
-      }
+    
+    if (event.target.closest(".btn-call")) {
+        
+        const serviceName =
+        event.target.closest(".btn-call").parentNode.parentNode.childNodes[5]
+        .innerText;
+        
+        const serviceNumber =
+        event.target.closest(".btn-call").parentNode.parentNode.childNodes[7]
+        .innerText;
+        
+        //showing both alert and implementing reducing coin condition
+        const coinCount = Number(document.getElementById("coin-count").innerText);
+        
+        if (coinCount >= 20) {
+            //showing alert for calling
+            alert(" 📞 Calling " + serviceName + " " + serviceNumber);
+            
+            const newCoinCount = coinCount - 20;
+            
+            document.getElementById("coin-count").innerText = newCoinCount;
+            //creating the history element
+            const newHistoryElement = document.createElement("div");
+            
+            newHistoryElement.innerHTML = `
+            <div class="bg-[#fafafa] p-4 flex justify-between items-center rounded-3xl my-5">
+            <div>
+            <p class="font-semibold">${
+                event.target.closest(".btn-call").parentNode
+                .parentNode.childNodes[3].innerText
+            }</p>
+            <p>${serviceNumber}</p>
+            </div>
+            <div>
+            ${new Date().toLocaleTimeString()}
+            </div>
+            </div>
+            `;
+            document.getElementById("call-history").appendChild(newHistoryElement);
+        } else {
+            //showing alert for insufficient coin
+            alert("😭 Insufficient Coin.");
+        }
     }
-
+    
     //for copy button functionality
-    if (event.target.classList.contains("btn-copy")) {
+    if (event.target.closest(".btn-copy")) {
       //copying the service number to the clipboard
       const textToCopy =
-        event.target.parentNode.parentNode.childNodes[7].innerText;
+        event.target.closest(".btn-copy").parentNode.parentNode.childNodes[7].innerText;
 
       const textArea = document.createElement("textarea");
       textArea.value = textToCopy;
@@ -75,10 +80,12 @@ document
       //increasing copy count
       const copyCount = Number(document.getElementById("copy-count").innerText);
       const newCopyCount = copyCount + 1;
-      
+
       //showing alert for copy button
       alert(
-        event.target.parentNode.parentNode.childNodes[3].innerText + ` "${event.target.parentNode.parentNode.childNodes[7].innerText}"` + " copied."
+        event.target.closest(".btn-copy").parentNode.parentNode.childNodes[3].innerText +
+          ` "${event.target.closest(".btn-copy").parentNode.parentNode.childNodes[7].innerText}"` +
+          " copied."
       );
       document.getElementById("copy-count").innerText = newCopyCount;
     }
