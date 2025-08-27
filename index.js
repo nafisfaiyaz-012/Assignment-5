498a1453e9f7716753313d6b60e3af57139f29b0
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-08-27 13:03:36 +0600

    Call button functionality added with alert and reducing coin

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ document
       document.getElementById("heart-counter").innerText = newHeartCounter;
     }
 
+    //for call button it will run this
+    if (event.target.classList.contains("btn-call")) {
+      //showing alert
+      const serviceName =
+        event.target.parentNode.parentNode.childNodes[5].innerText;
+
+      const serviceNumber =
+        event.target.parentNode.parentNode.childNodes[7].innerText;
+
+      //reducing coin
+      const coinCount = Number(document.getElementById("coin-count").innerText);
+      if (coinCount >= 20) {
+        alert(" 📞 Calling " + serviceName + " " + serviceNumber);
+        const newCoinCount = coinCount - 20;
+        document.getElementById("coin-count").innerText = newCoinCount;
+      } else {
+        alert("😭 Insufficient Coin.");
+      }
+    }
     
   });
